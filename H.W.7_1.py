ac9f2bd26d3f10cc870b9cb4f539b46967bbfd62
Tetiana17 <tetianaosiievska@gmail.com>
2024-07-20 20:16:16 -0700

    Add files via upload

---
+++ H.W.7_1.py
@@ -0,0 +1,12 @@
+def say_hi(name, age):
+    print(f"Hi!My name is {name} and I'm {age} years old.")
+
+
+# Приклад для виклику функціі test1
+name_1 = "Alex"
+age_1 = "32"
+say_hi(name_1, age_1)
+# Приклад для виклику функціі test2
+name_2 = "Frank"
+age_2 = 68
+say_hi(name_2, age_2)
